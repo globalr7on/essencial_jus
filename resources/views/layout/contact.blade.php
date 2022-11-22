795733778f265502850de883332dc6769a3f4cf9
Victor Hugo <globalr7on@gmail.com>
2022-11-21 23:00:35 -0300

    Ajustes

--- resources/views/layout/contact.blade.php
+++ resources/views/layout/contact.blade.php
@@ -315,12 +315,23 @@
     <script>
        var map = L.map('map').setView([-25.4573122, -49.2987814], 13);
 
-       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+       googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3']
         }).addTo(map);
 
-        
+    
+
+    //    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //         maxZoom: 19,
+    //         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    //     }).addTo(map);
+
+        // let marker = L.marker([-25.4573122, -49.2987814]).addTo(map)
+
+        var marker = L.marker([-25.4573122, -49.2987814],
+            {alt: 'jus'}).addTo(map) // "Kyiv" is the accessible name of this marker
+            .bindPopup('<strong>Escritorio Curitiba:</strong> Essencial Jus');
     </script>
 </body>
 
